2bb4013de28fa0e50ecc1d287c4cf00bfe1da8fe
Bagas Aditya <bagasaditya486@gmail.com>
2026-04-23 21:59:48 +0700

    pertemuan 7

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+//package
+
+use Illuminate\Http\Request;
+//import
+
+class DosenController extends Controller
+{
+    //
+    public function index(){
+    return "<h1>Halo ini adalah method index, dalam controller DosenController. - www.malasngoding.com</h1>";
+}
+    public function biodata(){
+    $nama = "Mukhammad Bagas Aditya";
+    $umur = 20;
+    $pelajaran = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+
+    return view('biodata', ['nama' => $nama, 'umur' => $umur, 'matkul' => $pelajaran]);
+}
+}
