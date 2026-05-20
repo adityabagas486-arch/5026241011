26ceb7e95232e1a2352fe70d31503628be19dad0
Bagas Aditya <bagasaditya486@gmail.com>
2026-05-20 12:13:53 +0700

    pertemuan 12

---
+++ resources/views/aksesTugas.blade.php
@@ -0,0 +1,51 @@
+<html lang="en">
+
+<head>
+    <title>5026241011</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+    <div class="container mt-4 mb-4">
+        <center>
+            <h1>5026241011 Mukhammad Bagas Aditya</h1>
+
+            <h5>FE5026241011</h5>
+
+            <p>
+                <a href="/pert5" target="_blank" class="btn btn-primary">Pertemuan 5</a>
+            </p>
+            <p>
+                <a href="/newsc" target="_blank" class="btn btn-primary">News Copy</a>
+            </p>
+            <p>
+                <a href="/pert4" target="_blank" class="btn btn-primary">Pertemuan 4</a>
+            </p>
+            <p>
+                <a href="/respon" target="_blank" class="btn btn-primary">Responsive</a>
+            </p>
+            <p>
+                <a href="/typography" target="_blank" class="btn btn-primary">Tugas Typography</a>
+            </p>
+            <p>
+                <a href="/index" target="_blank" class="btn btn-primary">Index</a>
+            </p>
+            <p>
+                <a href="/linktree1" target="_blank" class="btn btn-primary">Link Tree</a>
+            </p>
+            <p>
+                <a href="/linktreec" target="_blank" class="btn btn-primary">Link Tree Copy</a>
+            </p>
+
+        </center>
+    </div>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\BlogController;
-use App\Http\Controllers\Pegawaiontroller;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,7 +25,7 @@ Route::get('pert5', function () {
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/pegawailama/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 //blog
@@ -60,3 +61,9 @@ Route::get('linktreec', function () {
 	return view('linktreecopy');
 });
 
+Route::get('/aksesTugas', function () {
+    return view('aksesTugas');
+});
+
+//route CRUD
+Route::get('/pegawai',[PegawaiDBController::class, 'index2']);
